8de3b89413f57db7ea651fa58ede47968ec4debb
Rowan <rmmeaney@mun.ca>
2020-07-28 22:31:31 -0230

    multi camera window code

--- imageCapture.py
+++ imageCapture.py
@@ -1,13 +1,62 @@
 import cv2
 
-vid = cv2.VideoCapture(0)
+cam1 = cv2.VideoCapture(0)
+cam2 = cv2.VideoCapture(1)
+cam3 = cv2.VideoCapture(2)
+cam4 = cv2.VideoCapture(3)
+cam5 = cv2.VideoCapture(4)
 
+# Camera 1
 while(True):
 
-    ret, frame = vid.read()
+    ret, frame = cam1.read()
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-vid.release()
+cam1.release()
+cv2.destroyAllWindows
+
+#Camera 2
+while(True):
+
+    ret, frame = cam2.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('w'):
+        break
+
+cam2.release()
+cv2.destroyAllWindows
+
+#Camera 3
+while(True):
+
+    ret, frame = cam3.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('e'):
+        break
+
+cam3.release()
+cv2.destroyAllWindows
+
+#Camera 4
+while(True):
+
+    ret, frame = cam4.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('f'):
+        break
+
+cam4.release()
+cv2.destroyAllWindows
+
+#Camera 5
+while(True):
+
+    ret, frame = cam5.read()
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('g'):
+        break
+
+cam5.release()
 cv2.destroyAllWindows
